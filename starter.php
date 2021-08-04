8bb754baba92649c6f0b79f20250ca77ea594382
Jailson Soares de Souza Filho <jailson.filho@techlead.local>
2021-08-04 14:45:35 -0300

    implementation

--- starter.php
+++ starter.php
@@ -1,7 +1,12 @@
 <?php
 
-use Src\domain\student\Student;
+use Src\shared\domain\event\EventPublisher;
 use Src\infra\student\RepositoryStudentMemo;
+use Src\academy\domain\student\StudentListnerLog;
+use Src\gamification\infra\seal\RepositorySealMemo;
+use Src\gamification\application\GenerateBeginnergSeal;
+use Src\academy\application\registerStudent\RegisterStudent;
+use Src\academy\application\registerStudent\DtoRegisterStudent;
 
 require 'vendor/autoload.php';
 
@@ -11,7 +16,11 @@ $email = $argv[3];
 $ddd = $argv[4];
 $number = $argv[5];
 
-$student = Student::withCpfNameEmail($cpf, $name, $email)->addPhone($ddd, $number);
+$publisher = new EventPublisher();
 
-$repository = new RepositoryStudentMemo();
-$repository->addStudent($student);
+$publisher->addListner(new StudentListnerLog());
+$publisher->addListner(new GenerateBeginnergSeal(new RepositorySealMemo()));
+
+$useCase = new RegisterStudent(new RepositoryStudentMemo(), $publisher);
+
+$useCase->exec(new DtoRegisterStudent($cpf, $name, $email));
