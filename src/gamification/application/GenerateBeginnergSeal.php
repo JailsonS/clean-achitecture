0d8ad88fd437529335e81da33202a3db434c0e61
Jailson Soares de Souza Filho <jailson.filho@techlead.local>
2021-08-04 14:44:53 -0300

    updates

--- src/gamification/application/GenerateBeginnergSeal.php
+++ src/gamification/application/GenerateBeginnergSeal.php
@@ -2,9 +2,18 @@
 namespace Src\gamification\application;
 
 use Src\shared\domain\event\EventListner;
+use Src\gamification\domain\seal\RepositorySeal;
 
 class GenerateBeginnergSeal extends EventListner
 {
+
+    private RepositorySeal $repositorySeal;
+
+    public function __construct(RepositorySeal $repositorySeal)
+    {
+        $this->repositorySeal = $repositorySeal;
+    }
+
     public function hasExecEvent(Event $event): bool
     {
         return $event->name() === 'aluno_matriculado';
@@ -12,6 +21,11 @@ class GenerateBeginnergSeal extends EventListner
 
     public function reactTo(Event $event): void
     {
+        $array = $event->jsonSerialize();
+        $cpf = $array['cpfStudent'];
 
-    }
+        $seal = new Seal($cpf, 'NOVATO');
+
+        $this->repositorySeal->add($seal);
+    }   
 }
